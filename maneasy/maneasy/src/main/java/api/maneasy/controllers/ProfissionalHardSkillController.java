1fd1c0aa10035379a54cb6d4f9b7d519a91f0c39
jhonatanferreira94 <jhonjhonpf@hotmail.com>
2023-11-28 22:29:36 -0300

    Atualizacao

--- maneasy/maneasy/src/main/java/api/maneasy/controllers/ProfissionalHardSkillController.java
+++ maneasy/maneasy/src/main/java/api/maneasy/controllers/ProfissionalHardSkillController.java
@@ -1,27 +1,48 @@
 package api.maneasy.controllers;
 
 import api.maneasy.models.ProfissionalHardSkillModel;
+import api.maneasy.models.ProfissionalModel;
 import api.maneasy.repositories.ProfissionalHardSkillRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
-@RequestMapping(value = "/", produces = {"application/json"})
+@RequestMapping(value = "/profissionalHardSkill", produces = {"application/json"})
 public class ProfissionalHardSkillController {
 
     @Autowired
     ProfissionalHardSkillRepository profissionaisHardSkillRepository;
-
+    @GetMapping
     public ResponseEntity<List<ProfissionalHardSkillModel>>listarProfissionaisHardSkill(){
         return ResponseEntity.status(HttpStatus.OK).body(profissionaisHardSkillRepository.findAll());
     }
 
+    @GetMapping("/{idProfssionalHardSkill}")
+    public ResponseEntity<Object> buscarProfissionalHardSkill(@PathVariable(value = "idProfssionalHardSkill") UUID id){
+        Optional<ProfissionalHardSkillModel> profissionalHardSkillBuscado = profissionaisHardSkillRepository.findById(id);
 
+        if (profissionalHardSkillBuscado.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Profissional / HardSkill não encontrado");
+        }
 
+        return ResponseEntity.status(HttpStatus.OK).body(profissionalHardSkillBuscado.get());
+    }
+
+    @DeleteMapping("/{idProfssionalHardSkill}")
+    public ResponseEntity<Object> deletarProfissionalHardSkill(@PathVariable(value = "idProfssionalHardSkill") UUID id){
+        Optional<ProfissionalHardSkillModel> profissionalHardSkillBuscado = profissionaisHardSkillRepository.findById(id);
+
+        if (profissionalHardSkillBuscado.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Profissional / HardSkill não encontrado");
+        }
+        profissionaisHardSkillRepository.delete(profissionalHardSkillBuscado.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Profissional / HardSkill deletado com sucesso!");
+    }
 
 }
--- maneasy/maneasy/src/main/java/api/maneasy/repositories/ProfissionalHardSkillRepository.java
+++ maneasy/maneasy/src/main/java/api/maneasy/repositories/ProfissionalHardSkillRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface ProfissionalHardSkillRepository extends JpaRepository<ProfissionalHardSkillModel, UUID> {
-    ProfissionalHardSkillModel findById_profissional ()
+    ProfissionalHardSkillModel findById_profissional ();
 
 }
